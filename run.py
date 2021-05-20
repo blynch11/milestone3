33cc3726310c9a29010462f4656a417a8bfa656c
blynch11 <blynch2491@gmail.com>
2021-05-20 15:47:15 +0000

    initial commit, setup base template, set up multiple pages for site, installed & imported flask

---
+++ run.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/beers")
+def beers():
+    return render_template("beers.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/breweries")
+def breweries():
+    return render_template("breweries.html")
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
